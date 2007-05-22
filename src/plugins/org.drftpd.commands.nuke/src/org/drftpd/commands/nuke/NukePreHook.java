aroberts3928/drftpd

commit a434425f380c4d1789d13375c8106aa932455619

JPF:  * Small fix on StandardCommandManager. (Proper handling of exceptions)  * Modified the behavior of SlaveSelection so that when it find an error it will throw an exception instead of continue loading the chain.  * Making DiskSelection pluggable. (pretty much the same thing that was done with SlaeSelection)
git-svn-id: https://drftpd.svn.sourceforge.net/svnroot/drftpd/branches/jpf@1736 c83153ee-f3e8-0310-b165-dc3a72e09794

src/slave/src/org/drftpd/slave/RootCollection.java
@@ -29,7 +29,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.drftpd.slave.diskselection.DiskSelection;
+import org.drftpd.slave.diskselection.DiskSelectionInterface;
 
 import se.mog.io.File;
 
@@ -41,12 +41,13 @@ public class RootCollection {
 	private static final Logger logger = Logger.getLogger(RootCollection.class);
 
 	private Collection<Root> _roots = null;
+	private DiskSelectionInterface _diskSelection = null;
 
-	public RootCollection(Collection<Root> roots) throws IOException {
+	public RootCollection(DiskSelectionInterface diskSelection, Collection<Root> roots) throws IOException {
 		/** sanity checks * */
 		validateRoots(roots);
 		_roots = new ArrayList<Root>(roots);
-		DiskSelection.init(this);
+		_diskSelection = diskSelection;
 	}
 	
 	/**
@@ -95,7 +96,7 @@ public class RootCollection {
 	 * @throws IOException
 	 */
 	public File getARootFileDir(String dir) throws IOException {
-		Root bestRoot = DiskSelection.getDiskSelection().getBestRoot(dir);
+		Root bestRoot = _diskSelection.getBestRoot(dir);
 
 		// to avoid this error SlaveSelectionManager MUST work
 		// synchronized with DiskSelection.

src/slave/src/org/drftpd/slave/Slave.java
@@ -55,7 +55,11 @@ import org.drftpd.slave.async.AsyncResponse;
 import org.drftpd.slave.async.AsyncResponseDiskStatus;
 import org.drftpd.slave.async.AsyncResponseException;
 import org.drftpd.slave.async.AsyncResponseTransferStatus;
+import org.drftpd.slave.diskselection.DiskSelectionInterface;
 import org.drftpd.util.PortRange;
+import org.java.plugin.PluginManager;
+import org.java.plugin.registry.Extension;
+import org.java.plugin.registry.ExtensionPoint;
 
 import se.mog.io.File;
 import se.mog.io.PermissionDeniedException;
@@ -107,6 +111,8 @@ public class Slave {
 	private boolean _sslMaster;
 	
 	private SlaveProtocolCentral _central;
+	
+	private DiskSelectionInterface _diskSelection = null;
 
 	protected Slave() {
 
@@ -189,7 +195,9 @@ public class Slave {
 		_uploadChecksums = p.getProperty("enableuploadchecksums", "true").equals("true");
 		_downloadChecksums = p.getProperty("enabledownloadchecksums", "true").equals("true");
 		_bufferSize = Integer.parseInt(p.getProperty("bufferSize", "0"));
+	
 		_roots = getDefaultRootBasket(p);
+		loadDiskSelection(p);
 
 		_transfers = new HashMap<TransferIndex, Transfer>();
 
@@ -201,9 +209,38 @@ public class Slave {
 			_portRange = new PortRange(_bufferSize);
 		}
 	}
+	
+	private void loadDiskSelection(Properties cfg) {
+		PluginManager manager = PluginManager.lookup(this);
+		ExtensionPoint exp = manager.getRegistry().getExtensionPoint("slave", "DiskSelection");
+		String desiredDiskSelection = PropertyHelper.getProperty(cfg, "diskselection");
+		
+		for (Extension ext : exp.getAvailableExtensions()) {
+			String pluginId = ext.getDeclaringPluginDescriptor().getId();
+			if (pluginId.equals(desiredDiskSelection)) {
+				ClassLoader classLoader = manager.getPluginClassLoader(ext.getDeclaringPluginDescriptor());
+				String className = ext.getParameter("Class").valueAsString();
 
-	public static RootCollection getDefaultRootBasket(Properties cfg)
-			throws IOException {
+				try {
+					if (!manager.isPluginActivated(ext.getDeclaringPluginDescriptor()))
+						manager.activatePlugin(pluginId);
+
+					Class clazz = classLoader.loadClass(className);
+					
+					_diskSelection = (DiskSelectionInterface) clazz
+											.getConstructor(new Class[] { Slave.class }).newInstance(new Object[] { this });
+				} catch (Exception e) {
+					throw new RuntimeException("Unable to load DiskSelection", e);
+				}
+			}
+		}
+		
+		if (_diskSelection == null) {
+			throw new RuntimeException("Unable to load DiskSelection");
+		}
+	}
+
+	public RootCollection getDefaultRootBasket(Properties cfg) throws IOException {
 		ArrayList<Root> roots = new ArrayList<Root>();
 
 		for (int i = 1; true; i++) {
@@ -217,7 +254,7 @@ public class Slave {
 			roots.add(new Root(rootString));
 		}
 
-		return new RootCollection(roots);
+		return new RootCollection(_diskSelection, roots);
 	}
 
 	public static void boot() throws Exception {

build.xml
@@ -42,6 +42,7 @@ check - checks plug-ins integrity
 		<ant dir="src/plugins/org.drftpd.usermanager.javabeans" target="${target}" />
 		<ant dir="src/plugins/org.drftpd.commandmanager" target="${target}" />
 		<ant dir="src/plugins/org.drftpd.slaveselection.filter" target="${target}" />
+		<ant dir="src/plugins/org.drftpd.slave.diskselection.filter" target="${target}" />
 		<ant dir="src/plugins/org.drftpd.commands.dataconnection" target="${target}" />
 		<ant dir="src/plugins/org.drftpd.commands.login" target="${target}" />
 		<ant dir="src/plugins/org.drftpd.commands.list" target="${target}" />

slave.conf.dist
@@ -39,6 +39,9 @@ bufferSize=0
 # 60000 = one minute
 slave.timeout=60000
 
+# DiskSelection plugin.
+# If doesn't know what that means, do NOT change this.
+diskselection=org.drftpd.slave.diskselection.filter
 
 # enabled cipher suites
 # if left empty, default set will be used

src/plugins/org.drftpd.commandmanager/src/org/drftpd/commandmanager/StandardCommandManager.java
@@ -259,33 +259,33 @@ public class StandardCommandManager implements CommandManagerInterface, EventSub
     		catch (InvocationTargetException e) {
     			throw e.getCause();
     		}
-    	}
-    	catch (Throwable e2) {
-    		if (e2 instanceof ImproperUsageException) {
-	    		response = StandardCommandManager.genericResponse("RESPONSE_501_SYNTAX_ERROR");
-	    		String helpString = request.getProperties().getProperty("help.specific");
-	    		if (helpString == null) {
-	    			response.addComment("Bug your siteop to add help for the \""
-							+ request.getCommand() + "\" command");
-	    		}
-	    		else {
-	    			ReplacerEnvironment env = new ReplacerEnvironment();
-	    			env.add("command", request.getCommand().toUpperCase());
-	    			try {
-	    				response.addComment(SimplePrintf.jprintf(helpString,env));
-	    			}
-	    			catch (FormatterException e) {
-	    				response.addComment(request.getCommand().toUpperCase()
-	    						+ " command has an invalid help.specific definition");
-	    			}
-	    		}
+    	} catch (ImproperUsageException e) {
+    		response = StandardCommandManager.genericResponse("RESPONSE_501_SYNTAX_ERROR");
+    		String helpString = request.getProperties().getProperty("help.specific");
+    		if (helpString == null) {
+    			response.addComment("Bug your siteop to add help for the \""
+						+ request.getCommand() + "\" command");
     		}
     		else {
+    			ReplacerEnvironment env = new ReplacerEnvironment();
+    			env.add("command", request.getCommand().toUpperCase());
+    			try {
+    				response.addComment(SimplePrintf.jprintf(helpString,env));
+    			}
+    			catch (FormatterException e1) {
+    				response.addComment(request.getCommand().toUpperCase()
+    						+ " command has an invalid help.specific definition");
+    			}
+    		}
+    	} catch (Throwable t) {
+    		if (!(t instanceof Error)) {
     			CommandResponseInterface cmdFailed = new CommandResponse(540, "Command execution failed");
-    			logger.error("Command "+request.getCommand()+" failed",e2);
+    			logger.error("Command "+request.getCommand()+" failed", t);
     			return cmdFailed;
     		}
+    		throw (Error) t;
     	}
+
     	commandContainer.getCommandInterfaceInstance().doPostHooks(request, response);
 		return response;
 	}

src/plugins/org.drftpd.slave.diskselection.filter/build.xml
@@ -1,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project name="org.drftpd.slave.diskselection.filter plug-in build file"
+         default="build"
+         basedir=".">
+	<jpf-info manifest="${basedir}/plugin.xml"
+		propertyid="plugin.id"
+		propertyversion="plugin.version" />
+	<property name="plugin.home"
+		value="${basedir}/../../../plugins/${plugin.id}" />
+	<copy todir="${plugin.home}" includeemptydirs="false">
+		<fileset dir="${basedir}"
+			excludes="classes/,.*,*build*,src/" />
+	</copy>
+	<jpf-path
+		basedir="${basedir}/../../.."
+		pathid="plugin.classpath"
+		pluginid="${plugin.id}"
+		includes="core/*/plugin.xml,plugins/*/plugin.xml"
+		verbose="true" />
+
+	<target name="build">
+		<echo>Building ${plugin.id} plug-in to folder ${plugin.home}</echo>
+		<mkdir dir="${plugin.home}/classes" />
+		<javac destdir="${plugin.home}/classes"
+			srcdir="${basedir}/src"
+			classpathref="plugin.classpath"
+			excludes="**/*Test*"
+			source="1.5"
+			target="1.5"
+			debug="true"
+			optimize="false">
+		</javac>
+		<copy todir="${plugin.home}/classes">
+			<fileset dir="${basedir}/src"
+				includes="**/*"
+				excludes="**/.*,**/*.java" />
+		</copy>
+	</target>
+</project>

src/plugins/org.drftpd.slave.diskselection.filter/plugin.xml
@@ -1,0 +1,46 @@
+<?xml version="1.0" ?>
+<!DOCTYPE plugin PUBLIC "-//JPF//Java Plug-in Manifest 1.0" "http://jpf.sourceforge.net/plugin_1_0.dtd">
+<plugin id="org.drftpd.slave.diskselection.filter" version="2.1.0">
+	<attributes>
+		<attribute id="DenyUnload" value="true" />
+	</attributes>
+	<requires>
+		<import plugin-id="slave" exported="true" />
+	</requires>
+	<runtime>
+		<library id="org.drftpd.slave.diskselection.filter" path="classes/" type="code">
+			<export prefix="*" />
+		</library>
+	</runtime>
+	
+	<extension-point id="DiskFilter">
+		<parameter-def id="FilterName" />
+		<parameter-def id="ClassName" />
+	</extension-point>
+	
+	<extension plugin-id="slave" point-id="DiskSelection" id="DiskSelectionFilter">
+		<parameter id="Class" value="org.drftpd.slave.diskselection.filter.DiskSelectionFilter"/>
+	</extension>
+	
+	<extension plugin-id="org.drftpd.slave.diskselection.filter" point-id="DiskFilter" id="SlaveCycleFilter">
+		<parameter id="ClassName" value="org.drftpd.slave.diskselection.filter.CycleFilter"/>
+		<parameter id="FilterName" value="Cycle"/>
+	</extension>
+	<extension plugin-id="org.drftpd.slave.diskselection.filter" point-id="DiskFilter" id="SlaveMatchdirFilter">
+		<parameter id="ClassName" value="org.drftpd.slave.diskselection.filter.MatchdirFilter"/>
+		<parameter id="FilterName" value="Matchdir"/>
+	</extension>
+	<extension plugin-id="org.drftpd.slave.diskselection.filter" point-id="DiskFilter" id="SlaveMinfreespaceFilter">
+		<parameter id="ClassName" value="org.drftpd.slave.diskselection.filter.MinfreespaceFilter"/>
+		<parameter id="FilterName" value="Minfreespace"/>
+	</extension>
+	<extension plugin-id="org.drftpd.slave.diskselection.filter" point-id="DiskFilter" id="SlavePriorityFilter">
+		<parameter id="ClassName" value="org.drftpd.slave.diskselection.filter.PriorityFilter"/>
+		<parameter id="FilterName" value="Priority"/>
+	</extension>
+	<extension plugin-id="org.drftpd.slave.diskselection.filter" point-id="DiskFilter" id="SlaveRandomspreadFilter">
+		<parameter id="ClassName" value="org.drftpd.slave.diskselection.filter.RandomspreadFilter"/>
+		<parameter id="FilterName" value="Randomspread"/>
+	</extension>
+
+</plugin>

commit 236a2ea0cf0ad52afe0db9590c4e1fd3e14e5188

JPF:  * Fixed bug that was introduced by r1736 which was causing some problems while uploading.  * Minor cleanup on Nuke PreHook.
git-svn-id: https://drftpd.svn.sourceforge.net/svnroot/drftpd/branches/jpf@1737 c83153ee-f3e8-0310-b165-dc3a72e09794

src/plugins/org.drftpd.commands.nuke/src/org/drftpd/commands/nuke/NukePreHook.java
@@ -17,7 +17,6 @@
  */
 package org.drftpd.commands.nuke;
 
-import org.apache.log4j.Logger;
 import org.drftpd.commandmanager.CommandRequest;
 import org.drftpd.commandmanager.CommandRequestInterface;
 import org.drftpd.commandmanager.CommandResponse;
@@ -32,17 +31,19 @@ import org.drftpd.vfs.DirectoryHandle;
  * @version $Id$
  */
 public class NukePreHook implements PreHookInterface {
-	private static final Logger logger = Logger.getLogger(NukePreHook.class);
-	
 	public void initialize(StandardCommandManager cManager) {
 	}
 	
 	public CommandRequestInterface doNukeCheck(CommandRequest request) {
-		logger.debug("doNukeCheck  called.");
 		String targetDir = request.getArgument();
 		DirectoryHandle currentDir = request.getCurrentDirectory();
-		String fullPath = currentDir.getPath()+"/"+targetDir;
-		logger.debug("FullPath = "+ fullPath); 
+		
+		String fullPath = "";
+		if (currentDir.getPath().equals("/")) // no need append the '/'
+			fullPath = currentDir.getPath()+targetDir;
+		else
+			fullPath = currentDir.getPath()+"/"+targetDir;
+
 		try {
 			NukeData nd = NukeBeans.getNukeBeans().get(fullPath);
 			// nuke found.

src/slave/src/org/drftpd/slave/RootCollection.java
@@ -29,7 +29,6 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.drftpd.slave.diskselection.DiskSelectionInterface;
 
 import se.mog.io.File;
 
@@ -41,13 +40,13 @@ public class RootCollection {
 	private static final Logger logger = Logger.getLogger(RootCollection.class);
 
 	private Collection<Root> _roots = null;
-	private DiskSelectionInterface _diskSelection = null;
+	private Slave _slave = null;
 
-	public RootCollection(DiskSelectionInterface diskSelection, Collection<Root> roots) throws IOException {
+	public RootCollection(Slave slave, Collection<Root> roots) throws IOException {
 		/** sanity checks * */
 		validateRoots(roots);
 		_roots = new ArrayList<Root>(roots);
-		_diskSelection = diskSelection;
+		_slave = slave;
 	}
 	
 	/**
@@ -96,7 +95,7 @@ public class RootCollection {
 	 * @throws IOException
 	 */
 	public File getARootFileDir(String dir) throws IOException {
-		Root bestRoot = _diskSelection.getBestRoot(dir);
+		Root bestRoot = _slave.getDiskSelection().getBestRoot(dir);
 
 		// to avoid this error SlaveSelectionManager MUST work
 		// synchronized with DiskSelection.

src/slave/src/org/drftpd/slave/Slave.java
@@ -240,6 +240,10 @@ public class Slave {
 		}
 	}
 
+	public DiskSelectionInterface getDiskSelection() {
+		return _diskSelection;
+	}
+	
 	public RootCollection getDefaultRootBasket(Properties cfg) throws IOException {
 		ArrayList<Root> roots = new ArrayList<Root>();
 
@@ -254,7 +258,7 @@ public class Slave {
 			roots.add(new Root(rootString));
 		}
 
-		return new RootCollection(_diskSelection, roots);
+		return new RootCollection(this, roots);
 	}
 
 	public static void boot() throws Exception {