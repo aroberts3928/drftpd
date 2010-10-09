aroberts3928/drftpd

commit 4f7f352fa47450c56edd40347a47fc188cb06621

Changed checkPathPermission to take argument with String instead of DirectoryHandle because Uploadex and all regex's for that matter would not look at filenames.  This fixes #346
git-svn-id: https://drftpd.svn.sourceforge.net/svnroot/drftpd/trunk@2155 c83153ee-f3e8-0310-b165-dc3a72e09794

src/master/src/org/drftpd/permissions/GlobPathPermission.java
@@ -23,7 +23,6 @@ import org.apache.oro.text.GlobCompiler;
 import org.apache.oro.text.regex.MalformedPatternException;
 import org.apache.oro.text.regex.Pattern;
 import org.apache.oro.text.regex.Perl5Matcher;
-import org.drftpd.vfs.DirectoryHandle;
 
 /**
  * @author mog
@@ -42,11 +41,8 @@ public class GlobPathPermission extends PathPermission {
 		_pat = new GlobCompiler().compile(pattern);		
 	}
 
-	public boolean checkPath(DirectoryHandle file) {
-		String path = file.getPath().concat("/");
-
+	public boolean checkPath(String path) {
 		Perl5Matcher m = new Perl5Matcher();
-
 		return m.matches(path, _pat);
 	}
 	

src/master/src/org/drftpd/config/ConfigManager.java
@@ -308,7 +308,7 @@ public class ConfigManager implements ConfigInterface {
 		
 		if (perms != null && !perms.isEmpty()) {
 			for (PathPermission perm : perms) {
-				if (perm.checkPath(path)) {
+				if (perm.checkPath(path.getPath().concat("/"))) {
 					return perm.check(user);
 				}
 			}

src/master/src/org/drftpd/permissions/PathPermission.java
@@ -19,8 +19,6 @@ package org.drftpd.permissions;
 
 import java.util.Collection;
 
-import org.drftpd.vfs.DirectoryHandle;
-
 /**
  * @author mog
  * @version $Id$
@@ -30,5 +28,5 @@ public abstract class PathPermission extends Permission {
 		super(users);
 	}
 
-	public abstract boolean checkPath(DirectoryHandle path);
+	public abstract boolean checkPath(String path);
 }

src/master/src/org/drftpd/permissions/RegexPathPermission.java
@@ -4,8 +4,6 @@ import java.util.Collection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.drftpd.vfs.DirectoryHandle;
-
 public class RegexPathPermission extends PathPermission {
     private Pattern _pat;
 
@@ -14,8 +12,8 @@ public class RegexPathPermission extends PathPermission {
         _pat = pat;
     }
 
-	public boolean checkPath(DirectoryHandle path) {
-		Matcher m = _pat.matcher(path.getPath());
+	public boolean checkPath(String path) {
+		Matcher m = _pat.matcher(path);
 		return m.find();
 	}
 	

src/master/src/org/drftpd/permissions/StringPathPermission.java
@@ -19,7 +19,6 @@ package org.drftpd.permissions;
 
 import java.util.Collection;
 
-import org.drftpd.vfs.DirectoryHandle;
 import org.drftpd.vfs.VirtualFileSystem;
 
 /**
@@ -34,7 +33,7 @@ public class StringPathPermission extends PathPermission {
 		_path = VirtualFileSystem.fixPath(path);
 	}
 
-	public boolean checkPath(DirectoryHandle path) {
-		return path.getPath().startsWith(_path);
+	public boolean checkPath(String path) {
+		return path.startsWith(_path);
 	}
 }

src/master/src/org/drftpd/vfs/DirectoryHandle.java
@@ -773,7 +773,7 @@ public class DirectoryHandle extends InodeHandle implements
 		
 		checkHiddenPath(newDir, user);
 		
-		if (!getVFSPermissions().checkPathPermission("makedir", user, newDir)) {
+		if (!getVFSPermissions().checkPathPermission("makedir", user, newDir.getPath())) {
 			throw new PermissionDeniedException("You are not allowed to create a directory at "+ newDir.getParent());
 		}
 		
@@ -843,7 +843,7 @@ public class DirectoryHandle extends InodeHandle implements
 		
 		checkHiddenPath(this, user);
 		
-		if (!getVFSPermissions().checkPathPermission("upload", user, this)) {
+		if (!getVFSPermissions().checkPathPermission("upload", user, this.getPath().concat("/").concat(name))) {
 			throw new PermissionDeniedException("You are not allowed to upload to "+ getParent());
 		}
 		

src/master/src/org/drftpd/vfs/InodeHandle.java
@@ -66,17 +66,21 @@ public abstract class InodeHandle implements InodeHandleInterface, Comparable<In
 		}
 		
 		DirectoryHandle dir = null;
+		String filename="";
 		if (this instanceof DirectoryHandle) {
 			dir = (DirectoryHandle) this;
 		} else {
 			dir = this.getParent();
+			if (this.getName() != null) {
+				filename = "/" + this.getName();
+			}
 		}
 		
 		checkHiddenPath(this, user);
 		
 		boolean allowed = false;
 		if (user.getName().equals(getUsername())) {
-			if (!getVFSPermissions().checkPathPermission("deleteown", user, dir)) {
+			if (!getVFSPermissions().checkPathPermission("deleteown", user, dir.getPath().concat(filename))) {
 				// the user owns the file althought it doesnt have enough perms to delete it.
 				throw new PermissionDeniedException("You are not allowed to delete "+getPath());
 			} else {
@@ -87,7 +91,7 @@ public abstract class InodeHandle implements InodeHandleInterface, Comparable<In
 			}
 		}
 
-		if (!allowed && !getVFSPermissions().checkPathPermission("delete", user, dir)) {
+		if (!allowed && !getVFSPermissions().checkPathPermission("delete", user, dir.getPath().concat(filename))) {
 			throw new PermissionDeniedException("You are not allowed to delete "+getPath());
 		}
 		
@@ -238,10 +242,14 @@ public abstract class InodeHandle implements InodeHandleInterface, Comparable<In
 		}
 		
 		DirectoryHandle dir = null;
+		String filename = "";
 		if (this instanceof DirectoryHandle) {
 			dir = (DirectoryHandle) this;
 		} else {
 			dir = this.getParent();
+			if (this.getName() != null) {
+				filename = "/" + this.getName();
+			}
 		}
 		
 		checkHiddenPath(this, user); // checking the current inode.
@@ -249,7 +257,7 @@ public abstract class InodeHandle implements InodeHandleInterface, Comparable<In
 		
 		boolean allowed = false;
 		if (user.getName().equals(getUsername())) {
-			if (!getVFSPermissions().checkPathPermission("renameown", user, dir)) {
+			if (!getVFSPermissions().checkPathPermission("renameown", user, dir.getPath().concat(filename))) {
 				// the user owns the file althought it doesnt have enough perms to rename it.
 				throw new PermissionDeniedException("You are not allowed to rename "+getPath());
 			} else {
@@ -260,7 +268,7 @@ public abstract class InodeHandle implements InodeHandleInterface, Comparable<In
 			}
 		}
 
-		if (!allowed && !getVFSPermissions().checkPathPermission("rename", user, dir)) {
+		if (!allowed && !getVFSPermissions().checkPathPermission("rename", user, dir.getPath().concat(filename))) {
 			throw new PermissionDeniedException("You are not allowed to rename "+getPath());
 		}
 		
@@ -335,7 +343,7 @@ public abstract class InodeHandle implements InodeHandleInterface, Comparable<In
 		
 		DirectoryHandle dir = inode.isDirectory() ? (DirectoryHandle) inode : inode.getParent();
 		
-		if (getVFSPermissions().checkPathPermission("privpath", user, dir, false, true)) {
+		if (getVFSPermissions().checkPathPermission("privpath", user, dir.getPath(), false, true)) {
 			// this has to mirror what is said in
 			// VirtualFileSystemDirectory.getInodeByName()
 			throw new FileNotFoundException("FileNotFound: " + inode.getName()

src/master/src/org/drftpd/vfs/perms/VFSPermissions.java
@@ -30,7 +30,6 @@ import org.drftpd.permissions.PathPermission;
 import org.drftpd.usermanager.User;
 import org.drftpd.util.CommonPluginUtils;
 import org.drftpd.util.PluginObjectContainer;
-import org.drftpd.vfs.DirectoryHandle;
 
 /**
  * This object handles all the permissions releated to the VFS.
@@ -156,15 +155,15 @@ public class VFSPermissions {
 		list.add(pathPerm);
 	}
 	
-	public boolean checkPathPermission(String type, User user, DirectoryHandle path) {
+	public boolean checkPathPermission(String type, User user, String path) {
 		return checkPathPermission(type, user, path, false);
 	}
 
-	public boolean checkPathPermission(String type, User user, DirectoryHandle path, boolean defaults) {
+	public boolean checkPathPermission(String type, User user, String path, boolean defaults) {
 		return checkPathPermission(type, user, path, defaults, false);
 	}
 	
-	public boolean checkPathPermission(String type, User user, DirectoryHandle path, boolean defaults, boolean invertUserSemantic) {
+	public boolean checkPathPermission(String type, User user, String path, boolean defaults, boolean invertUserSemantic) {
 		
 		if (!verifyType(type)) {
 			throw new IllegalArgumentException("Invalid VFS perm type.");

commit 1694ae9051d08bc2ed3633fcddddde120c0ccaa8

Fixing a bug introduced by r2155 and r2156
git-svn-id: https://drftpd.svn.sourceforge.net/svnroot/drftpd/trunk@2165 c83153ee-f3e8-0310-b165-dc3a72e09794

src/master/src/org/drftpd/permissions/GlobPathPermission.java
@@ -44,7 +44,13 @@ public class GlobPathPermission extends PathPermission {
 
 	public boolean checkPath(InodeHandle inode) {
 		Perl5Matcher m = new Perl5Matcher();
-		return m.matches(inode.getPath(), _pat);
+		
+		String path = inode.getPath();
+		if (inode.isDirectory() && !inode.getPath().endsWith("/")) {
+			path = inode.getPath().concat("/");
+		}
+		
+		return m.matches(path, _pat);
 	}
 	
 	public String toString() {